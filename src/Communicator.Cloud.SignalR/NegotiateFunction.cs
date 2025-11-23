328e1d47795fa8bbdeb2dcd84aaec07d172f1654
Nikhil S Thomas <nikhil.s.thomas5@gmail.com>
2025-11-23 19:23:33 +0530

    Added new group function app

--- src/Communicator.Cloud.SignalR/NegotiateFunction.cs
+++ src/Communicator.Cloud.SignalR/NegotiateFunction.cs
@@ -8,95 +8,40 @@
 
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.SignalRService;
+using Microsoft.Extensions.Logging;
 using System.Net;
 using System.Threading.Tasks;
-using System.Web;
-using System.Collections.Specialized;
 
 namespace Communicator.Cloud.SignalR;
 
 /// <summary>
-/// Azure Function that handles SignalR negotiation and returns connection info.
+/// Class to handle SignalR negotiation requests.
 /// </summary>
 public class NegotiateFunction
 {
-    /// <summary>
-    /// Logger instance for logging information
-    /// </summary>
     private readonly ILogger<NegotiateFunction> _logger;
 
-    /// <summary>
-    /// Constructor to initialize the logger.
-    /// </summary>
-    /// <param name="logger">Used to instantiate logger</param>
     public NegotiateFunction(ILogger<NegotiateFunction> logger)
     {
         _logger = logger;
     }
 
     /// <summary>
-    /// Response datastructure containing both SignalR output and HTTP response.
+    /// Function app endpoint to handle negotiation requests.
     /// </summary>
-    public class NegotiateResponse
-    {
-        [SignalROutput(HubName = "meetingHub")]
-        public object? SignalROutput { get; set; }
-
-        [HttpResult]
-        public HttpResponseData? HttpResponse { get; set; }
-    }
-
-    /// <summary>
-    /// HTTP-triggered function that generates SignalR connection info
-    /// and automatically adds the user to a SignalR group.
-    /// </summary>
+    /// <param name="req">HTTP request</param>
+    /// <param name="connectionInfo">Auto-generated SignalR connection info</param>
     [Function("negotiate")]
-    public async Task<NegotiateResponse> Run(
-    [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-    [SignalRConnectionInfoInput(HubName = "meetingHub")] SignalRConnectionInfo connectionInfo)
+    public async Task<HttpResponseData> Negotiate(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [SignalRConnectionInfoInput(HubName = "meetingHub", UserId = "{userId}")] SignalRConnectionInfo connectionInfo)
     {
-        try
-        {
-            var query = HttpUtility.ParseQueryString(req.Url.Query);
-            string? meetingId = query["meetingId"];
+        _logger.LogInformation("Negotiation request received.");
 
-            _logger.LogInformation($"Negotiation request received. MeetingId={meetingId}");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(connectionInfo);
 
-            var groupAction = new SignalRGroupAction(SignalRGroupActionType.Add)
-            {
-                UserId = meetingId,
-                GroupName = meetingId
-            };
-
-            var http = req.CreateResponse(HttpStatusCode.OK);
-            await http.WriteAsJsonAsync(new
-            {
-                url = connectionInfo.Url,
-                accessToken = connectionInfo.AccessToken,
-                meetingId
-            });
-
-            // IMPORTANT: return array to satisfy output binding
-            return new NegotiateResponse
-            {
-                SignalROutput = new object[] { connectionInfo, groupAction },
-                HttpResponse = http
-            };
-        }
-        catch (Exception ex)
-        {
-            // This reveals the true underlying problem
-            _logger.LogError(ex, "NEGOTIATE FAILED: {Message}", ex.Message);
-
-            var resp = req.CreateResponse(HttpStatusCode.InternalServerError);
-            await resp.WriteStringAsync(ex.ToString());
-            return new NegotiateResponse
-            {
-                HttpResponse = resp
-            };
-        }
+        return response;
     }
-
 }
